97b40f3512c8aaa66eace389c3cf124d5ba3d30e
VanillaFox <71337244+VanillaFox@users.noreply.github.com>
2024-12-11 02:01:13 +0300

    feat: init

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {TuiRoot} from '@taiga-ui/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, TuiRoot],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.less'
+})
+export class AppComponent {
+  title = 'habits-tracker';
+}
---
+++ src/app/habits-list/habits-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-habits-list',
+  imports: [],
+  templateUrl: './habits-list.component.html',
+  styleUrl: './habits-list.component.less',
+  standalone: true
+})
+export class HabitsListComponent {
+
+}
